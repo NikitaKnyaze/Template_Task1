dbcb8c63653840ba8e763c7791e692c56a23ffc7
Knyazev Nikita <nk8222138@gmail.com>
2024-05-13 21:14:52 +0300

    first commit

---
+++ Task1_squaring/Task1_squaring.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <vector>
+
+template <typename T>
+T sqr(T a)
+{
+   return a * a;
+};
+
+template<typename T>
+void sqr(std::vector<T>& a)
+{
+
+   for (int i = 0; i < size(a); ++i)
+   {
+      a[i] = a[i] * a[i];
+   }
+}
+
+int main()
+{
+   int some = 8;
+   std::vector<int> some_v{-1, 4, 8};
+   std::cout << sqr(some)<<std::endl;
+   sqr(some_v);
+   for(const auto c : some_v)
+   std::cout << c << " ";
+   return 0;
+}
